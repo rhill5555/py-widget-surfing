fd7952dc336365998049a181dac7686d57b33c2c
heather <queenheather32@gmail.com>
2022-03-15 22:57:59 -0400

    Changed QWidget to QMainWindow

--- gui/main_widget/main_widget.py
+++ gui/main_widget/main_widget.py
@@ -1,14 +1,14 @@
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 
-from gui.main_widget.ui_to_py.main import Ui_Form
+from gui.main_widget.ui_to_py.wsl_analytics_ui import Ui_MainWindow
 
 
-class MainWidget(QWidget, Ui_Form):
+class MainWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         # Call the constructor for the inherited QWidget class.
-        QWidget.__init__(self)
+        QMainWindow.__init__(self)
 
         # Call the setupUi function that adds all the pyqt stuff to this class, that was designed in the designer.
         # This function is inherited from the Ui_Form class.
@@ -18,10 +18,11 @@ class MainWidget(QWidget, Ui_Form):
         self.connect_slots()
 
     def connect_slots(self):
-        self.cont_entry.currentIndexChanged.connect(self.slot_cont_entry_on_selection_change)
+        #self.cont_entry.currentIndexChanged.connect(self.slot_cont_entry_on_selection_change)
+        pass
 
-    def slot_cont_entry_on_selection_change(self):
-        print(self.cont_entry.currentText())
+   # def slot_cont_entry_on_selection_change(self):
+        #print(self.cont_entry.currentText())
 
 
 if __name__ == '__main__':
