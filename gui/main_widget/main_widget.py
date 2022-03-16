d4b3249cc1adca8d8d05db23f4bd9c2eb67c721b
heather <queenheather32@gmail.com>
2022-03-15 23:21:17 -0400

    setup enums for example

--- gui/main_widget/main_widget.py
+++ gui/main_widget/main_widget.py
@@ -1,8 +1,36 @@
 import sys
+from enum import Enum
 
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 
 from gui.main_widget.ui_to_py.wsl_analytics_ui import Ui_MainWindow
+
+
+class Places:
+    class Continent(Enum):
+        Africa = 1
+        North_America = 2
+
+    class Africa(Enum):
+        Eastern_Cape = ["Eastern_Cape"]
+
+    class NorthAmerica(Enum):
+        USA = ["Hawaii", "California"]
+        Mexico = ["Ox"]
+
+    class Hawaii(Enum):
+        PipeLine = 1
+        Sunset_Beach = 2
+
+    class California(Enum):
+        San_Clemente = 1
+        Hunting_Beach = 2
+
+    class Ox(Enum):
+        Ox_beach = 1
+
+    class Eastern_Cape(Enum):
+        Jeffreys_Bay = 1
 
 
 class MainWidget(QMainWindow, Ui_MainWindow):
