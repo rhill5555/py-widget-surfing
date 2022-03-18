e511b06f3b3d336aafc3a5b9703b20baf7fc75b6
heather <queenheather32@gmail.com>
2022-03-18 01:13:04 -0400

    Moved Places class to separate script

--- gui/main_widget/main_widget.py
+++ gui/main_widget/main_widget.py
@@ -1,5 +1,5 @@
 import sys
-from enum import Enum
+from src.place_hier import Places
 from typing import Optional, List
 
 import mysql.connector
@@ -15,92 +15,9 @@ from gui.common_widget.dialog_widget.simple_lineedit import SimpleLineEdit
 # mycursor.execute("select * from wsl.continents")
 # myresult = mycursor.fetchall()
 
-########################################################################################################################
-# Define Places
-class Places:
-    @staticmethod
-    def continent(mysql_connection: MySQLConnection):
-        mycursor = mysql_connection.cursor()
-        mycursor.execute("select continent from wsl.continents")
-        result = mycursor.fetchall()
-
-        cont_lst = []
-        for x in result:
-            cont_lst.append(x)
-
-        return cont_lst
-
-    @staticmethod
-    def countries(mysql_connection: MySQLConnection, continent: str):
-        mycursor = mysql_connection.cursor()
-        mycursor.execute(f"""select country.country
-                         from wsl.countries country
-                         join wsl.continents cont
-                              on country.continent_id = cont.id
-                         where cont.continent = '{continent}'""")
-        result = mycursor.fetchall()
-
-        country_lst = []
-        for x in result:
-            country_lst.append(x)
-
-        return country_lst
-
-    @staticmethod
-    def region(mysql_connection: MySQLConnection, country: str):
-        mycursor = mysql_connection.cursor()
-        mycursor.execute(f"""select region.region
-                         from wsl.regions region
-                         join wsl.countries country
-                              on region.country_id = country.id
-                         where country.country = '{country}'""")
-        result = mycursor.fetchall()
-
-        region_lst = []
-        for x in result:
-            region_lst.append(x)
-
-        return region_lst
-
-    @staticmethod
-    def city(mysql_connection: MySQLConnection, region: str):
-        mycursor = mysql_connection.cursor()
-        mycursor.execute("select city from wsl.continents")
-        result = mycursor.fetchall()
-
-        city_lst = []
-        for x in result:
-            city_lst.append(x)
-
-        return city_lst
-
-    @staticmethod
-    def surf_break(mysql_connection: MySQLConnection, city: str):
-        pass
-
-    class Africa(Enum):
-        Eastern_Cape = ["Eastern_Cape"]
-
-    class NorthAmerica(Enum):
-        USA = ["Hawaii", "California"]
-        Mexico = ["Ox"]
-
-    class Hawaii(Enum):
-        PipeLine = 1
-        Sunset_Beach = 2
-
-    class California(Enum):
-        San_Clemente = 1
-        Hunting_Beach = 2
-
-    class Ox(Enum):
-        Ox_beach = 1
-
-    class Eastern_Cape(Enum):
-        Jeffreys_Bay = 1
-
 
 ########################################################################################################################
+
 class MainWidget(QMainWindow, Ui_MainWindow):
     def __init__(self, sql_host: str, sql_user: str, sql_password: str):
         # Call the constructor for the inherited QWidget class.
@@ -185,39 +102,14 @@ class MainWidget(QMainWindow, Ui_MainWindow):
     def slot_break_country_cb_on_index_change(self):
         self.BreakRegionCb.clear()
         self.BreakRegionCb.addItems([item[0] for item in Places.region(mysql_connection=self.mysql,
-                                                                           country=self.BreakCountryCb.currentText())])
+                                                                          country=self.BreakCountryCb.currentText())])
 
 
     # This is the event handler (slot) for the combobox "breakregioncb" changing index.
     def slot_break_region_cb_on_index_change(self):
-        if self.BreakRegionCb.currentText() == "Eastern_Cape":
-            # Clear the City Combobox.
-            self.BreakCityCb.clear()
-            # Add the regions.
-            self.BreakCityCb.addItems(
-                [item.name for item in Places.Eastern_Cape]
-            )
-        elif self.BreakRegionCb.currentText() == "California":
-            # Clear the City Combobox.
-            self.BreakCityCb.clear()
-            # Add the regions.
-            self.BreakCityCb.addItems(
-                [item.name for item in Places.California]
-            )
-        elif self.BreakRegionCb.currentText() == "Hawaii":
-            # Clear the City Combobox.
-            self.BreakCityCb.clear()
-            # Add the regions.
-            self.BreakCityCb.addItems(
-                [item.name for item in Places.Hawaii]
-            )
-        elif self.BreakRegionCb.currentText() == "Ox":
-            # Clear the City Combobox.
-            self.BreakCityCb.clear()
-            # Add the regions.
-            self.BreakCityCb.addItems(
-                [item.name for item in Places.Ox]
-            )
+        self.BreakCityCb.clear()
+        self.BreakCityCb.addItems([item[0] for item in Places.city(mysql_connection=self.mysql,
+                                                                       region=self.BreakRegionCb.currentText())])
 
     # This is the event handler (slot) for the submit button being clicked.
     def slot_break_submit_on_clicked(self):
