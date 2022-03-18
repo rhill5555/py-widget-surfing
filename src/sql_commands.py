c780e8f17f78703e93fc9ba6f29cf54a45ae49f6
heather <queenheather32@gmail.com>
2022-03-18 17:44:26 -0400

    Moved Places and Sql_Commands to one file

--- src/sql_commands.py
+++ src/sql_commands.py
@@ -1,11 +1,67 @@
 from mysql.connector import MySQLConnection
 
-class SqlComm:
+
+class Places:
     @staticmethod
-    def append_to_table(mysql_connection: MySQLConnection, table: str, columns: dict):
+    def continent(mysql_connection: MySQLConnection):
         mycursor = mysql_connection.cursor()
-        mycursor.execute(f"""INSERT INTO '{table}' (continent) VALUES
-                             ('{columns.name}', {columns.dataType}); 
-                             """)
+        mycursor.execute("select continent from wsl.continents")
         result = mycursor.fetchall()
 
+        cont_lst = []
+        for x in result:
+            cont_lst.append(x)
+
+        return cont_lst
+
+    @staticmethod
+    def countries(mysql_connection: MySQLConnection, continent: str):
+        mycursor = mysql_connection.cursor()
+        mycursor.execute(f"""select country.country
+                         from wsl.countries country
+                         join wsl.continents cont
+                              on country.continent_id = cont.id
+                         where cont.continent = '{continent}'""")
+        result = mycursor.fetchall()
+
+        country_lst = []
+        for x in result:
+            country_lst.append(x)
+
+        return country_lst
+
+    @staticmethod
+    def region(mysql_connection: MySQLConnection, country: str):
+        mycursor = mysql_connection.cursor()
+        mycursor.execute(f"""select region.region
+                         from wsl.regions region
+                         join wsl.countries country
+                              on region.country_id = country.id
+                         where country.country = '{country}'""")
+        result = mycursor.fetchall()
+
+        region_lst = []
+        for x in result:
+            region_lst.append(x)
+
+        return region_lst
+
+    @staticmethod
+    def city(mysql_connection: MySQLConnection, region: str):
+        mycursor = mysql_connection.cursor()
+        mycursor.execute(f"""select city.city
+                         from wsl.cities city
+                         join wsl.regions region
+                              on city.region_id = region.id
+                         where region.region = '{region}'""")
+        result = mycursor.fetchall()
+
+        city_lst = []
+        for x in result:
+            city_lst.append(x)
+
+        return city_lst
+
+    @staticmethod
+    def surf_break(mysql_connection: MySQLConnection, city: str):
+        pass
